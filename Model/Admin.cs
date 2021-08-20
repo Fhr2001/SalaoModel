9060bc21a1db8c1b1ccd500fe92b076076beb6f4
Ninorp <rodolfo.520@live.com>
2021-08-20 11:36:15 -0300

    Adding new structure

---
+++ Model/Admin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salão_Model.Model
+{
+    class Admin
+    {
+    }
+}
---
+++ WinForm/Inicial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Salão_Model.Form
+{
+    public partial class Inicial : Form
+    {
+        public Inicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
